134b88bb42b615e5547999d02b0dfa607feb74f5
Robert De La Torre <robertdrums64@gmail.com>
2025-01-29 16:11:45 -0800

    class tutorial

---
+++ test.py
@@ -0,0 +1,53 @@
+#
+# Example file for working with classes
+# LinkedIn Learning Python course by Joe Marini
+#
+
+# BASE CLASS
+class Vehicle():
+    def __init__(self, bodystyle):
+        self.bodystyle = bodystyle
+
+    def drive(self, speed):
+        self.mode = "driving"
+        self.speed = speed
+
+# VEHICLE SUB CLASS
+class Car(Vehicle):
+    def __init__(self, enginetype):
+        super().__init__("Car")
+        self.wheels = 4
+        self.doors = 4
+        self.enginetype = enginetype
+
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my", self.enginetype, "car at", self.speed)
+
+# MOTORCYCLE SUB CLASS
+class Motorcycle(Vehicle):
+    def __init__(self, enginetype, hassidecar):
+        super().__init__("Motorcyle")
+        if(hassidecar):
+            self.wheels = 3
+        else:
+            self.wheels = 2
+            self.doors = 0
+            self.enginetype = enginetype
+
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my", self.enginetype, "motorcycle at", self.speed)
+
+# MY VEHICLES
+car1 = Car("gas")
+car2 = Car("electric")
+mc1 = Motorcycle("gas", True)
+
+print(mc1.wheels)
+print(car1.enginetype)
+print(car2.doors)
+
+car1.drive(30)
+car2.drive(40)
+mc1.drive(50)
